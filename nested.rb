0058feb34e0492536f238c5ecbcce58a68924f90
Stephen Rivera <st.rivera_94@yahoo.com>
2019-08-22 13:41:54 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -104,10 +104,11 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-  return programmer_hash[:alan_kay][:known_for] = alans_new_info
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
 end
+
 
 def adding_to_dennis
 	programmer_hash = 
@@ -125,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      } 
-     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+      programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+     programmer_hash
 end
